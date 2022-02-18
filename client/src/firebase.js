4cd3a7dcb2d9ab397a4326e432f53de2682ca6bb
Rianto <rianto.rnt@gmail.com>
2022-02-18 19:59:46 +0800

    initialize firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD75rUMhSU347uFCkMSditPdITZNh30CI4",
+    authDomain: "rnt-computer-app.firebaseapp.com",
+    projectId: "rnt-computer-app",
+    storageBucket: "rnt-computer-app.appspot.com",
+    messagingSenderId: "656084539502",
+    appId: "1:656084539502:web:036fcdf215560329a02e7f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Export
+  export const auth = firebase.auth();
+  export const googleAuthProvider = new firebase.auth.googleAuthProvider();
