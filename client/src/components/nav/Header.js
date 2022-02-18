c39513970f8213156ba61003074422abfc5f5a2d
Rianto <rianto.rnt@gmail.com>
2022-02-18 14:35:05 +0800

    Ant design icons part 2

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,26 +1,34 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
-import { AppstoreOutlined,  UserAddOutlined } from '@ant-design/icons';
+import { HomeOutlined, UserAddOutlined, UserOutlined, LoginOutlined } from '@ant-design/icons';
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState('');
+  const [current, setCurrent] = useState('home');
 
   const handleClick = (e) => {
-    // 
+    // console.log(e.key)
     setCurrent(e.key);
   };
 
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Menu.Item key="mail" icon={<AppstoreOutlined />}>
+      <Item key="home" icon={<HomeOutlined />}>
         Home
-      </Menu.Item>
+      </Item>
 
-      <SubMenu key="SubMenu" icon={<UserAddOutlined />} title="Register">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      <Item key="login" icon={<LoginOutlined />} className="float-sm-right">
+        Login
+      </Item>
+
+      <Item key="register" icon={<UserAddOutlined />} className="float-sm-right">
+        Register
+      </Item>
+
+      <SubMenu icon={<UserOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
       </SubMenu>
     </Menu>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
+  // </React.StrictMode>,
   document.getElementById('root'),
 );
 
