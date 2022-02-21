044a860425e48e0f81ab500f4f118c6173b62c78
Rianto <rianto.rnt@gmail.com>
2022-02-21 15:20:39 +0800

    user logout feature

--- client/src/components/nav/Header.jsx
+++ client/src/components/nav/Header.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
-import { HomeOutlined, UserAddOutlined, UserOutlined, LoginOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  UserAddOutlined,
+  UserOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
 
@@ -12,14 +21,33 @@ const centerStyle = {
 };
 
 const rightStyleRegister = { position: "absolute", top: 0, right: 0 };
-const rightStyleLogin = { display: "flex-end", justifyContent: "space-between", position: "absolute", top: 0, right: 120 };
+const rightStyleLogin = {
+  display: "flex-end",
+  justifyContent: "space-between",
+  position: "absolute",
+  top: 0,
+  right: 120,
+};
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
 
+  let distpatch = useDispatch();
+  let history = useHistory();
+
   const handleClick = (e) => {
     // console.log(e.key)
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    distpatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+
+    history.push("/login");
   };
 
   return (
@@ -39,6 +67,9 @@ const Header = () => {
       <SubMenu style={centerStyle} key="username" icon={<UserOutlined />} title="Username">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
+        <Item key="logout" icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
       </SubMenu>
     </Menu>
   );
