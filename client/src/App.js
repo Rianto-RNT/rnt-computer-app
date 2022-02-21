58a3559dc0ac13852ba3b2742f6a4e6907f8930c
Rianto <rianto.rnt@gmail.com>
2022-02-21 12:26:15 +0800

    user in redux state - user login email and token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,7 +9,33 @@ import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            name: user.email,
+            token: idTokenResult.token
+          },
+        });
+      }
+    });
+
+    //Cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
