ccc6a56dc7ade769283026dbb68d6cffeaaa28a6
Rianto <rianto.rnt@gmail.com>
2022-02-23 19:51:03 +0800

    api user response - save user login to database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,24 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-      success: true,
-      data: 'Hit it!!! create-or-update-user',
-    });
-  };
+const User = require('../models/user');
 
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name, picture },
+    { new: true }
+  );
+
+  if (user) {
+    console.log('User Updated', user)
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name,
+      picture,
+    }).save();
+    console.log('User Created', newUser)
+    res.json(newUser);
+  }
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,20 @@
 const admin = require('../firebase/index');
 
-exports.protect = (req, res, next) => {
-  console.log(req.headers);
-  next();
+exports.protect = async (req, res, next) => {
+  // console.log(req.headers);
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+
+    // console.log('Firebase user in Protect', firebaseUser);
+
+    req.user = firebaseUser;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      error: 'Invalid or expired token',
+    });
+  }
 };
