6c711b1019f66b4330276c73beefcf3f9a3ea3e6
Rianto <rianto.rnt@gmail.com>
2022-03-09 12:10:28 +0800

    collapse description in admin dashboard

--- client/src/components/cards/AdminProductCard.jsx
+++ client/src/components/cards/AdminProductCard.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Card, Collapse } from "antd";
 
 const { Meta } = Card;
+const { Panel } = Collapse;
 
 const AdminProductCard = ({ product }) => {
   const { title, description, images } = product;
 
+  const [text, setText] = useState("");
+
   return (
     <Card
-      cover={<img src={images && images.length ? images[0].url : ""} style={{ height: "150px", objectFit: "cover" }} className="m-2" />}
+      cover={<img src={images && images.length ? images[0].url : ""} style={{ height: "150px", objectFit: "cover" }} className="p-1" />}
     >
-      <Meta title={title} description={description} />
+      <Meta title={title} />
+      <br />
+      <Collapse bordered={false} defaultActiveKey={["1"]} onChange={(e) => setText(e.target)}>
+        <Panel showArrow={true} header="Read more" key="2">
+          <p>description={description}</p>
+        </Panel>
+      </Collapse>
     </Card>
   );
 };
