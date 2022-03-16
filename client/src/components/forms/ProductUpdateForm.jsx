485199d8ebdfdea964aa306cff27518693d92748
Rianto <rianto.rnt@gmail.com>
2022-03-16 17:07:18 +0800

    Product update refresher

--- client/src/components/forms/ProductUpdateForm.jsx
+++ client/src/components/forms/ProductUpdateForm.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { Select, Input, Divider } from "antd";
+import { Select } from "antd";
 
 // Ant Design
-const { TextArea } = Input;
 const { Option } = Select;
-const dividerStyle = {
-  marginBottom: 2,
-  float: 'right'
-};
 
 const ProductUpdateForm = ({
   handleSubmit,
@@ -21,139 +16,175 @@ const ProductUpdateForm = ({
   setArrayOfSubcategory,
   selectedCategory,
 }) => {
-  const { title, description, price, category, shipping, quantity, colors, brands, color, brand } = values;
+  const { title, description, price, category, subcategory, shipping, quantity, images, colors, brands, color, brand } = values;
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Title
-        </Divider>
-        <input type="text" name="title" className="form-control" value={title} onChange={handleChange} />
+      {/* <!-- ROW-1 OPEN --> */}
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="card">
+            <div className="card-body">
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Product Name :</label>
+                <div className="col-md-9">
+                  <input
+                    type="text"
+                    name="title"
+                    className="form-control"
+                    placeholder="Product Name"
+                    value={title}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              {/* <!-- Row --> */}
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Product Description :</label>
+                <div className="col-md-9">
+                  <div className="form-group">
+                    <textarea
+                      type="text"
+                      name="description"
+                      className="form-control mb-4"
+                      placeholder="Maximum 2000 character"
+                      rows="4"
+                      value={description}
+                      onChange={handleChange}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              {/* <!--End Row--> */}
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Price :</label>
+                <div className="col-md-9">
+                  <input type="number" name="price" className="form-control" value={price} onChange={handleChange} />
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Shipping :</label>
+                <div className="col-md-9">
+                  <select
+                    value={shipping === "yes" ? "yes" : "no"}
+                    name="shipping"
+                    className="form-control form-select select2"
+                    onChange={handleChange}
+                  >
+                    <option value="please-select" disabled>
+                      --Select--
+                    </option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Quantity :</label>
+                <div className="col-md-9">
+                  <input type="number" name="quantity" className="form-control" value={quantity} onChange={handleChange} />
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Color :</label>
+                <div className="col-md-9">
+                  <select value={color} name="color" className="form-control form-select select2" onChange={handleChange}>
+                    <option value="please-select" disabled>
+                      --Select--
+                    </option>
+
+                    {colors.map((c) => (
+                      <option key={c} value={c}>
+                        {c}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Brand :</label>
+                <div className="col-md-9">
+                  <select value={brand} name="brand" className="form-control form-select select2" onChange={handleChange}>
+                    <option value="please-select" disabled>
+                      --Select--
+                    </option>
+
+                    {brands.map((b) => (
+                      <option key={b} value={b}>
+                        {b}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Category :</label>
+                <div className="col-md-9">
+                  <select
+                    value={selectedCategory ? selectedCategory : category._id}
+                    name="category"
+                    className="form-control form-select select2"
+                    onChange={handleCategoryChange}
+                  >
+                    <option disabled>--Select--</option>
+
+                    {categories.length > 0 &&
+                      categories.map((c) => (
+                        <option key={c._id} value={c._id}>
+                          {c.name}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <label className="col-md-3 form-label">Subcategory :</label>
+                <div className="col-md-9">
+                  <Select
+                    mode="multiple"
+                    allowClear
+                    style={{ width: "100%" }}
+                    placeholder="Please select"
+                    value={arrayOfSubcategory}
+                    onChange={(value) => setArrayOfSubcategory(value)}
+                  >
+                    {subcategoryOptions.length &&
+                      subcategoryOptions.map((s) => (
+                        <Option key={s._id} value={s._id}>
+                          {s.name}
+                        </Option>
+                      ))}
+                  </Select>
+                </div>
+              </div>
+
+              
+            </div>
+
+            <div className="card-footer">
+              {/* <!--Row--> */}
+              <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-9">
+                  <button className="btn btn-primary">Add Product</button>
+
+                  <button className="btn btn-default float-end">Discard</button>
+                </div>
+              </div>
+              {/* <!--End Row--> */}
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Description
-        </Divider>
-        <TextArea
-          rows={4}
-          placeholder="Maximum 2000 character"
-          maxLength={2000}
-          type="text"
-          name="description"
-          value={description}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Price
-        </Divider>
-        <input type="number" name="price" className="form-control" value={price} onChange={handleChange} />
-      </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Shipping
-        </Divider>
-        <select value={shipping === "yes" ? "yes" : "no"} name="shipping" className="form-control" onChange={handleChange}>
-          <option value="please-select" disabled>
-            --Please select--
-          </option>
-          <option value="no">No</option>
-          <option value="yes">Yes</option>
-        </select>
-      </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Quantity
-        </Divider>
-        <input type="number" name="quantity" className="form-control" value={quantity} onChange={handleChange} />
-      </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Color
-        </Divider>
-        <select value={color} name="color" className="form-control" onChange={handleChange}>
-          <option value="please-select" disabled>
-            --Please select--
-          </option>
-
-          {colors.map((c) => (
-            <option key={c} value={c}>
-              {c}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Brand
-        </Divider>
-        <select value={brand} name="brand" className="form-control" onChange={handleChange}>
-          <option value="please-select" disabled>
-            --Please select--
-          </option>
-
-          {brands.map((b) => (
-            <option key={b} value={b}>
-              {b}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="form-group">
-        <Divider orientation="left" style={dividerStyle}>
-          Category
-        </Divider>
-        <select
-          name="category"
-          className="form-control"
-          onChange={handleCategoryChange}
-          value={selectedCategory ? selectedCategory : category._id}
-        >
-          <option value="please-select" disabled>
-            --Please select--
-          </option>
-          {categories.length > 0 &&
-            categories.map((c) => (
-              <option key={c._id} value={c._id}>
-                {c.name}
-              </option>
-            ))}
-        </select>
-      </div>
-
-      <div>
-        <Divider orientation="left" style={dividerStyle}>
-          Subcategory
-        </Divider>
-        <Select
-          mode="multiple"
-          allowClear
-          style={{ width: "100%" }}
-          placeholder="Please select"
-          value={arrayOfSubcategory}
-          onChange={(value) => setArrayOfSubcategory(value)}
-        >
-          {subcategoryOptions.length &&
-            subcategoryOptions.map((s) => (
-              <Option key={s._id} value={s._id}>
-                {s.name}
-              </Option>
-            ))}
-        </Select>
-      </div>
-
-      <br />
-
-      <button className="btn btn-outline-info">Save</button>
+      {/* <!-- /ROW-1 CLOSED --> */}
     </form>
   );
 };
