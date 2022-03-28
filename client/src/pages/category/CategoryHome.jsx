ef2437b63f3e2629d05d1550619d1c7d014568ba
Rianto <rianto.rnt@gmail.com>
2022-03-28 22:46:23 +0800

    Category page with product

--- client/src/pages/category/CategoryHome.jsx
+++ client/src/pages/category/CategoryHome.jsx
@@ -11,14 +11,48 @@ const CategoryHome = ({ match }) => {
   const { slug } = match.params;
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     setLoading(true);
-    getSingleCategory(slug).then((c) => {
-      console.log(JSON.stringify(c.data, null, 4));
-      setSingleCategory(c.data);
+    getSingleCategory(slug).then((res) => {
+      setSingleCategory(res.data.category);
+      setProducts(res.data.products);
+      setLoading(false);
     });
   }, []);
 
-  return <p className="pt-12">{slug}</p>;
+  return (
+    <div className="main-container container-fluid">
+      <div className="page-header pt-6">
+        <div className="col-xl-12">
+          <div className="card text-info bg-info-transparent card-transparent">
+            <div className="card-body p-4">
+              {loading ? (
+                <h4>Loading...</h4>
+              ) : (
+                <h4>
+                  Hi, we got {products.length} product with category name of "{singleCategory.name}"
+                </h4>
+              )}
+            </div>
+          </div>
+
+          {products.map((p) => (
+            <div className="row" key={p._id}>
+              <div className="col-sm-2 col-xl-3">
+                <div className="card">
+                  <div className="card-body h-100">
+                    <div className="product-grid6">
+                      <ProductCard product={p} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryHome;
