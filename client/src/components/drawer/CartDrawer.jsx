3e8cfa714ca74e78cd41e9b418ed7bd92243f805
Rianto <rianto.rnt@gmail.com>
2022-04-08 19:10:30 +0800

    go to cart button in drawer

--- client/src/components/drawer/CartDrawer.jsx
+++ client/src/components/drawer/CartDrawer.jsx
@@ -12,7 +12,7 @@ const CartDrawer = () => {
   return (
     <Drawer
       className="text-center"
-      title={`Cart / Product Total: ${cart.length} items )`}
+      title={`Cart / Product: ${cart.length} items`}
       bordered="true"
       onClose={() => {
         dispatch({
@@ -27,6 +27,7 @@ const CartDrawer = () => {
         <Space>
           <Link to="/cart">
             <Button
+            type="primary"
               onClick={() =>
                 dispatch({
                   type: "SET_VISIBLE",
@@ -34,34 +35,36 @@ const CartDrawer = () => {
                 })
               }
             >
-              <i className="fe fe-shopping-cart me-2"></i>Show calendar
+              <i className="fe fe-shopping-cart me-2"></i>Go to Cart<i className="fe fe-arrow-right ms-1"></i>
             </Button>
           </Link>
         </Space>
       }
     >
-      {cart.map((p) => (
-        <div key={p._id} className="card-body">
-          <div className="d-flex overflow-visible">
-            {p.images[0] ? (
-              <img className="avatar bradius avatar-xl me-4 p-2 bg-white border" src={p.images[0].url} alt="avatar-img" />
-            ) : (
-              <img className="avatar bradius avatar-xl me-4 p-2 bg-white border" src={noImages} alt="avatar-img" />
-            )}
-            <div className="media-body valign-middle">
-              <Link to={`/product/${p.slug}`} className="fw-semibold text-dark">
-                {p.title}
-              </Link>
-              <div className="mb-1 text-warning">
-                {p && p.ratings && p.ratings.length > 0 ? productAverageRatings(p) : "No Ratings Found"}
+      <div className="card">
+        {cart.map((p) => (
+          <div key={p._id} className="card-body">
+            <div className="d-flex overflow-visible">
+              {p.images[0] ? (
+                <img className="avatar bradius avatar-xl me-4 p-2 bg-white border" src={p.images[0].url} alt="avatar-img" />
+              ) : (
+                <img className="avatar bradius avatar-xl me-4 p-2 bg-white border" src={noImages} alt="avatar-img" />
+              )}
+              <div className="media-body valign-middle">
+                <Link to={`/product/${p.slug}`} className="fw-semibold text-dark">
+                  {p.title}
+                </Link>
+                <div className="mb-1 text-warning">
+                  {p && p.ratings && p.ratings.length > 0 ? productAverageRatings(p) : "No Ratings Found"}
+                </div>
+                <h5 className="fw-bold">
+                  <p>Rp. {p.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                </h5>
               </div>
-              <h5 className="fw-bold">
-                <p>Rp. {p.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-              </h5>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </Drawer>
   );
 };
