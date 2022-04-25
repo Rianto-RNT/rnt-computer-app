a23db64429412a8ae482d48b721d868a9b5ec47f
Rianto <rianto.rnt@gmail.com>
2022-04-25 14:01:00 +0800

    order schema

---
+++ server/models/Order.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+      },
+      count: Number,
+      color: String,
+    },
+  ],
+  paymentIntent: {},
+  orderStatus: {
+    type: String,
+    default: 'Not Processed',
+    enum: [
+      'Not Processed',
+      'Processing',
+      'Dispatched',
+      'Cancelled',
+      'Completed',
+    ],
+  },
+  orderedBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
