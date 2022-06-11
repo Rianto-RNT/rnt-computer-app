42cf4f34ed939e3b70bbbb9fde940584999f684b
Rianto <rianto.rnt@gmail.com>
2022-06-12 00:03:21 +0800

    Loading to redirect refresher

--- client/src/utils/routes/LoadingToRedirect.jsx
+++ client/src/utils/routes/LoadingToRedirect.jsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Spin } from "antd";
 
+import AdminNav from "../../components/nav/AdminNav";
+
+const styles = {
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  alignItems: "center",
+  justifyContent: "center",
+};
+
 const LoadingToRedirect = () => {
   const [count, setCount] = useState(5);
   let history = useHistory();
@@ -17,10 +29,21 @@ const LoadingToRedirect = () => {
   }, [count, history]);
 
   return (
-    <div className="container p-5 text-center">
-      <Spin size="large" tip="Please wait, we redirect you in moment..." />
-      <br />
-      <p> {count} </p>
+    <div className="main-container container-fluid">
+      <div className="row row-cards">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+
+        <div className="col-md-10">
+          <div className="page-header pt-7" style={styles}>
+            <div className="list">
+              <Spin size="large" tip="Please wait, we redirect you in moment..." />
+              <h6 className="text-center"> {count} </h6>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
